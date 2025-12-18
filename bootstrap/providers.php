182ded0c41da3881b2408d83a9e9c59d60150e72
Marc Hannappel <marc-hannappel@outlook.de>
2025-12-18 13:21:02 +0100

    Adds SEO tools and spam protection
    Integrates SEO tools for improved search engine optimization and adds spam protection to enhance security.
    
    Includes packages for sitemap generation, honeypot spam prevention, mail preview, and error solutions to improve development workflow and application maintainability.

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,7 +1,10 @@
 <?php
 
+use Artesaos\SEOTools\Providers\SEOToolsServiceProvider;
+
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
     App\Providers\HorizonServiceProvider::class,
+    SEOToolsServiceProvider::class,
 ];
